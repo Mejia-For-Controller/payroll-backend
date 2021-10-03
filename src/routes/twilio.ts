413ae5384cfd963b5eda976d3bf49d69114d919b
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-10-02 23:56:18 -0700

    req

--- src/routes/twilio.ts
+++ src/routes/twilio.ts
@@ -2,14 +2,7 @@ import express from 'express'
 import twilio from 'twilio'
 export let twiliorouter = express.Router();
 twiliorouter.all('/', twilio.webhook(), function(req, res, next) {
-    console.log(req)
-      // Create a TwiML response
-      var twiml = new twilio.twiml.MessagingResponse()
-      twiml.message('Hello from node.js!');
-  
-      // Render the TwiML response as XML
-      res.type('text/xml');
-      res.send(twiml.toString());
+    console.log(req.body)
 });
 
 twiliorouter.all('/incomingmsg/:campaignid', twilio.webhook(), function(req, res, next) {
@@ -17,6 +10,7 @@ twiliorouter.all('/incomingmsg/:campaignid', twilio.webhook(), function(req, res
     //do a lookup of the campaign, see if it exists
 
         // if it exists, add the text message to the text database
+    res.send("Success")
 });
 
 twiliorouter.all('/statuscallback/:campaignid', twilio.webhook(), function(req, res, next) {
@@ -25,4 +19,5 @@ twiliorouter.all('/statuscallback/:campaignid', twilio.webhook(), function(req, 
     //do a lookup of the campaign, see if it exists
 
         // if it exists, update the status of that specific message
+        res.send("Success")
 });
