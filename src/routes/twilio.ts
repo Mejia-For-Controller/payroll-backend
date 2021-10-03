10afcd13aab839a30ac538eab28b0b0cee1c3bc2
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-10-03 00:56:00 -0700

    dfdfaldsjslkf

--- src/routes/twilio.ts
+++ src/routes/twilio.ts
@@ -1,11 +1,16 @@
 import express from 'express'
 import twilio from 'twilio'
 export let twiliorouter = express.Router();
-twiliorouter.all('/', twilio.webhook(), function(req, res, next) {
-    console.log(req.body)
-});
+var twilioFormat = function (req, res, next) {
+    if (!req.header('X-Twilio-Signature')) {
+        return res.type('text/plain')
+          .status(400)
+          .send('No signature header error - X-Twilio-Signature header does not exist, maybe this request is not coming from Twilio.');
+    }
+    next()
+}
 
-twiliorouter.post('/voice', twilio.webhook(), (req, res) => {
+twiliorouter.post('/voice', [twilioFormat], (req, res) => {
     // Twilio Voice URL - receives incoming calls from Twilio
     const response = new twilio.twiml.VoiceResponse();
   
@@ -16,15 +21,6 @@ twiliorouter.post('/voice', twilio.webhook(), (req, res) => {
     res.set('Content-Type', 'text/xml');
     res.send(response.toString());
 });
-  
-var twilioFormat = function (req, res, next) {
-    if (!req.header('X-Twilio-Signature')) {
-        return res.type('text/plain')
-          .status(400)
-          .send('No signature header error - X-Twilio-Signature header does not exist, maybe this request is not coming from Twilio.');
-    }
-    next()
-}
 
 twiliorouter.all('/incomingmsg/:campaignid', [twilioFormat], function (req, res, next) {
     console.log(req.body)
