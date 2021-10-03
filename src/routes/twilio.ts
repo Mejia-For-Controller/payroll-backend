af0785062245e689fa2be6667d094832ae28875f
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-10-03 00:46:08 -0700

    twilio format checker

--- src/routes/twilio.ts
+++ src/routes/twilio.ts
@@ -5,7 +5,28 @@ twiliorouter.all('/', twilio.webhook(), function(req, res, next) {
     console.log(req.body)
 });
 
-twiliorouter.all('/incomingmsg/:campaignid', twilio.webhook(), function(req, res, next) {
+twiliorouter.post('/voice', twilio.webhook(), (req, res) => {
+    // Twilio Voice URL - receives incoming calls from Twilio
+    const response = new twilio.twiml.VoiceResponse();
+  
+    response.say(
+      `Thanks for calling Mejia For Controller! Our volunteers are currently busy and will return your call shortly. Goodbye!`
+    );
+  
+    res.set('Content-Type', 'text/xml');
+    res.send(response.toString());
+});
+  
+var twilioFormat = function (req, res, next) {
+    if (!req.header('X-Twilio-Signature')) {
+        return res.type('text/plain')
+          .status(400)
+          .send('No signature header error - X-Twilio-Signature header does not exist, maybe this request is not coming from Twilio.');
+    }
+    next()
+}
+
+twiliorouter.all('/incomingmsg/:campaignid', [twilioFormat], function (req, res, next) {
     console.log(req.body)
     //do a lookup of the campaign, see if it exists
 
@@ -13,7 +34,7 @@ twiliorouter.all('/incomingmsg/:campaignid', twilio.webhook(), function(req, res
     res.send("Success")
 });
 
-twiliorouter.all('/statuscallback/:campaignid', twilio.webhook(), function(req, res, next) {
+twiliorouter.all('/statuscallback/:campaignid', [twilioFormat], function(req, res, next) {
     console.log(req.body)
 
     //do a lookup of the campaign, see if it exists
