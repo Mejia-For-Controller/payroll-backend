d644f9fc967da4804430a9708d4f5a850062f311
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-10-02 22:36:28 -0700

    twilio update

--- src/routes/twilio.ts
+++ src/routes/twilio.ts
@@ -3,5 +3,11 @@ import twilio from 'twilio'
 export let twiliorouter = express.Router();
 twiliorouter.all('/', twilio.webhook(), function(req, res, next) {
     console.log(req)
-    res.end('Success');
+      // Create a TwiML response
+      var twiml = new twilio.TwimlResponse();
+      twiml.message('Hello from node.js!');
+  
+      // Render the TwiML response as XML
+      res.type('text/xml');
+      res.send(twiml.toString());
 });
