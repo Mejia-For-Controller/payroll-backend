b336a3ab9873470039aff62e586cd40eeae00017
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2021-10-03 17:21:26 -0700

    cassandra

---
+++ src/createDatabases.ts
@@ -0,0 +1,34 @@
+import {cassandraclient} from './cassandra'
+
+
+export async function createDatabases() {
+  //This Function will automatically create the adorastats keyspace if it doesn't exist, otherwise, carry on
+  await cassandraclient.execute("CREATE KEYSPACE IF NOT EXISTS texter WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy',  'datacenter1': 1  };")
+      .then(async result => {
+         // await logger.discordDebugLogger.debug({ type: "cassandraclient", result: result })
+          /*console.log(result)*/
+      }).catch(error => console.error(error));
+
+  //Goes inside adora moderation keyspace, makes the table "trackedytvideoids"
+  await cassandraclient.execute("CREATE TABLE IF NOT EXISTS texter.campaigns (campaignid text PRIMARY KEY, name text, ownerid text, iconURL text, bannerURL text, creationtime timeuuid);")
+      .then(async result => {
+         // await logger.discordDebugLogger.debug({ type: "cassandraclient", result: result })
+          /*console.log(result)*/
+      }).catch(error => console.error(error));
+  
+      await cassandraclient.execute("CREATE TABLE IF NOT EXISTS texter.memberships (campaignid text PRIMARY KEY, userid text, joinedtime timeuuid);")
+      .then(async result => {
+         // await logger.discordDebugLogger.debug({ type: "cassandraclient", result: result })
+          /*console.log(result)*/
+      }).catch(error => console.error(error));
+
+  //Goes inside adorastats keyspace, makes the table "ytvideostats"
+  /*
+  await cassandraclient.execute("CREATE TABLE IF NOT EXISTS texter.messages (campaignid text PRIMARY KEY, content text, twilionumber text, campaignauthorid text);")
+      .then(async result => {
+          //await logger.discordDebugLogger.debug({ type: "cassandraclient", result: result })
+      }).catch(error => console.error(error));*/
+
+      //add paint the town to the list of default videos
+ // await addVideoToTrackList("_EEo-iE5u_A",undefined)
+}
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,17 @@ import express from 'express';
 import cors from 'cors'
 const app = express();
 const port = 3000; // default port to listen
-import {twiliorouter} from "./routes/twilio";
+import { twiliorouter } from "./routes/twilio";
+import * as admin from 'firebase-admin';
+const serviceAccount = require("./../serviceAccountKey.json");
+import {createDatabases} from './createDatabases'
+import { config } from './../config.json'
+
+createDatabases()
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 app.use(cors())
 app.use('/twilio', twiliorouter);
@@ -13,7 +23,49 @@ app.use(express.urlencoded())
 app.all("/", (req, res) => {
     console.log(req)
     res.send( "Hello world!" );
-} );
+});
+
+app.all('/mycampaigns', (req, res) => {
+    const sessionCookie = req.cookies.session || "";
+    admin
+    .auth()
+    .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+      .then(() => {
+      
+        // look up membership
+
+        return res.send({
+         "campaignlist": 
+          []
+      })
+    })
+    .catch((error) => {
+      //res.redirect("/login");
+      return res.type('text/plain')
+      .status(401)
+      .send('Invalid');
+    });
+})
+
+app.all('/createcampaign', (req, res) => {
+    const sessionCookie = req.cookies.session || "";
+    admin
+    .auth()
+    .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+        .then(() => {
+        
+            // check if the name doen't exist, then create it with the user id as the owner.
+        return res.send({
+             "success": true
+      })
+    })
+    .catch((error) => {
+      //res.redirect("/login");
+      return res.type('text/plain')
+      .status(400)
+      .send('Invalid');
+    });
+})
 
 // start the Express server
 app.listen( port, () => {
