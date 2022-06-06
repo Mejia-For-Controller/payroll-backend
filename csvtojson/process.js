dffe80100095468945a04cec1c24b782a264f8fb
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-06-06 00:11:53 -0700

    employees2021 refresh

--- csvtojson/process.js
+++ csvtojson/process.js
@@ -9,14 +9,33 @@ let onenineemployees = editJsonFile(`${__dirname}/employees2019.json`);
 let twozeroemployees = editJsonFile(`${__dirname}/employees2020.json`);
 let twooneemployees = editJsonFile(`${__dirname}/employees2021.json`);
 
+const letterCase = require('capitalize-decapitalize')
+
+const capitalizeString = require('capitalize-string')
+
+function replaceJob(stringjob) {
+    if (stringjob) {
+        if (stringjob.match(/ (i|ii|iii|iv|v|vi|vii|viii|ix|x)$/i)) {
+            return stringjob.replace(/ (i|ii|iii|iv|v|vi|vii|viii|ix|x)$/i,stringjob.match(/ (i|ii|iii|iv|v|vi|vii|viii|ix|x|xi)$/i)[0])
+         } else {
+             return stringjob;
+         }
+    } else {
+        console.log('wtf is this', stringjob)
+        return stringjob;
+    }
+   
+}
+
 var pathsToProcess = [
+    /*
     ['./2018.csv',oneeightemployees],
     ['./2019.csv',onenineemployees],
-    ['./2020.csv',twozeroemployees],
-    ['./2021.csv',twooneemployees],
+    ['./2020.csv',twozeroemployees],*/
+    ['./payroll-2022-new.csv',twooneemployees],
 ]
 
-const csv = require('csvtojson')
+const csv = require('csvtojson');
 
 pathsToProcess.forEach((eachYear) => {
     csv(
@@ -31,17 +50,26 @@ pathsToProcess.forEach((eachYear) => {
         .then((jsonObj) => {
             console.log(jsonObj);
             var correctedJson = jsonObj.map((eachEmployee) => {
-                eachEmployee['base'] = parseFloat(eachEmployee['base'])
-                eachEmployee['overtime'] = parseFloat(eachEmployee['overtime'])
-                eachEmployee['other'] = parseFloat(eachEmployee['other'])
-                if (eachEmployee['healthcare']) {
-                    eachEmployee['healthcare'] = parseFloat(eachEmployee['healthcare'])
+                eachEmployee['first'] = eachEmployee['first'] || eachEmployee['First']
+                eachEmployee['last'] = eachEmployee['last'] || eachEmployee['Last']
+                eachEmployee['jobtitle'] = replaceJob(eachEmployee['jobtitle'] || eachEmployee['Job'])
+                eachEmployee['dept'] = eachEmployee['dept'] || eachEmployee['Dept']
+                eachEmployee['base'] = parseFloat(eachEmployee['base'] || eachEmployee['Base Pay'])
+                eachEmployee['overtime'] = parseFloat(eachEmployee['overtime'] || eachEmployee['Overtime'])
+                eachEmployee['other'] = parseFloat(eachEmployee['other'] || parseFloat(eachEmployee['Other']))
+                if (eachEmployee['healthcare'] || eachEmployee['Health']) {
+                    eachEmployee['healthcare'] = parseFloat(eachEmployee['healthcare'] || eachEmployee['Health'])
                 }
-                if (eachEmployee['retirement']) {
+                if (eachEmployee['retirement'] || eachEmployee['Retirement']) {
                     
-                eachEmployee['retirement'] = parseFloat(eachEmployee['retirement'])
+                eachEmployee['retirement'] = parseFloat(eachEmployee['Retirement'] || eachEmployee['retirement'])
                 }
     
+                delete eachEmployee['First']
+                delete eachEmployee['Last']
+                delete eachEmployee['Job']
+                delete eachEmployee['Health']
+
                 return eachEmployee
             })
             eachYear[1].set("employees", correctedJson);
